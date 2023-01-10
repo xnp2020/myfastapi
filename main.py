764e1181ed0de3e5db990b80ef4913734700eb48
xianianping <xianianping@tongfutech.cn>
2023-01-10 17:34:16 +0800

    deps

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
 from datetime import datetime
-from fastapi.encoders import jsonable_encoder
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, File, UploadFile
-from fastapi import FastAPI, Form
-from fastapi import FastAPI, status
-from typing import Union
-from pydantic import BaseModel, EmailStr
-from fastapi import FastAPI, Header
 from enum import Enum
+from typing import Union
 
-from fastapi import Body, Cookie, FastAPI, Path, Query
-from pydantic import BaseModel, Field, HttpUrl
+from fastapi import (Body, Cookie, Depends, FastAPI, File, Form, Header,
+                     HTTPException, Path, Query, UploadFile, status)
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, EmailStr, Field, HttpUrl
 
 app = FastAPI()
 
@@ -627,15 +622,76 @@ def update_item(id: str, ab: Ab):
     fake_db[id] = json_compatible_item_data
 
 
+# 依赖注入
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
 
 
-@app.get("/coms/")
+@app.get("/coms/", tags=["Depends"])
 async def read_items(commons: dict = Depends(common_parameters)):
     return commons
 
 
-@app.get("/users/")
+@app.get("/users/", tags=["Depends"])
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
+
+
+fake_items_db2 = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/fines/", tags=["Depends"])
+# Depends依赖注入只需要参数为一个callable，而class也是callable，故可以直接注入class，Depends没有参数则默认采用声明的class，即Depends()=Depends(CommonQueryParams)
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db2[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+
+def query_extractor(q: str | None = None):
+    return q
+
+
+# 依赖嵌套
+def query_or_cookie_extractor(
+    q: str = Depends(query_extractor), last_query: str | None = Cookie(default=None)
+):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/read_query/", tags=["Depends"])
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
+
+
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get("/deps/", dependencies=[Depends(verify_token), Depends(verify_key)], tags=["Depends"])
+# 依赖需要执行，但不需要依赖返回值
+async def deps():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+# 整个app都注入依赖
+# app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
